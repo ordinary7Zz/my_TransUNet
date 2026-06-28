a5e27e21d08644240aa42f175b662003e6014ae4
bodongwang <bodongwang@tencent.com>
2026-06-28 20:56:33 +0800

    edit

--- infer_dir.sh
+++ infer_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python infer_dir.py \
-  --ckpt ./model/TU_Synapse224/TU_pretrain_R50-ViT-B_16_skip3_epo10_bs2_224/epoch_9.pth \
+  --ckpt ./my_model/TG_Video/epoch_4.pth \
   --img_dir /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/sample/images \
   --out_dir ./inference/sample \
   --img_size 224 \
